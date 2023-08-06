da266b7263e81d090d5e8a7ee847e85d0b276c4b
vinay-works <vinayoffficial937@gmail.com>
2023-08-06 20:21:36 +0530

    commit

--- server.js
+++ server.js
@@ -10,16 +10,22 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.send('Hello World!');
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    const first = parseInt(req.body.first);
+    const second = parseInt(req.body.second);
+    const sum = first + second;
+    res.json({ "result": sum });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    let first = parseInt(req.body.first);
+    let second = parseInt(req.body.second);
+    let diff = first - second;
+    res.json({ "result": diff });
 });
 
 app.use(baseUrl, baseRouter);
